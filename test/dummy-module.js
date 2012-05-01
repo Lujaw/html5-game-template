2a686837110f35ae12a8e32087bed58dda71fddf
Dan Mosedale <dmose@meer.net>
2012-05-01 12:13:46 -0700

    Dummy module doesn't even need an exports sta assignment to make node-qunit happy.  Replaced that with a detailed comment explaining what's going on here.

--- test/dummy-module.js
+++ test/dummy-module.js
@@ -1,2 +1,9 @@
-exports.dummyFunction = function() {};
-
+// node-qunit expects that the code under test is going to be in a 
+// JS file that can be reasonably attached to the appropriate scope
+// along with the tests to be run.  In our case, the code being tested
+// is invoked by exec() volo which executes code found in the local
+// volofile.  So this is a dummy file to placate node-qunit.
+//
+// TD should talk to jrburke about whether some other structure 
+// (like calling into volo as a library rather than doing an exec)
+// makes sense.
